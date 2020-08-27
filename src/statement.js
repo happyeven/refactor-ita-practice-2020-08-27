d6c991189b8ac6451b5546958e678a39af992e27
Dong <1361840595@qq.com>
2020-08-27 15:20:10 +0800

    [refactor] [dong] refactor : change the extracted methods to global methods

--- src/statement.js
+++ src/statement.js
@@ -16,46 +16,48 @@ function statement (invoice, plays) {
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 
-  function formatUSD(thisAmount){
-    const format = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format;
-    return format(thisAmount)
-  }
+  
+}
 
-  function getCredit(perf, play) {
-    let credit = 0;
-    // add volume credits
-    credit += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type)
-      credit += Math.floor(perf.audience / 5);
-    return credit;
-  }
+function formatUSD(thisAmount){
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+  return format(thisAmount)
+}
 
-  function getAmount(play, perf) {
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    return thisAmount;
+function getCredit(perf, play) {
+  let credit = 0;
+  // add volume credits
+  credit += Math.max(perf.audience - 30, 0);
+  // add extra credit for every ten comedy attendees
+  if ('comedy' === play.type)
+    credit += Math.floor(perf.audience / 5);
+  return credit;
+}
+
+function getAmount(play, perf) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
   }
+  return thisAmount;
 }
 
 module.exports = {
