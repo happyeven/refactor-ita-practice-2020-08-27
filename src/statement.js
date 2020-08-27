c101aa017c950b816b39f1a3e59d5c10cf058128
Dong <1361840595@qq.com>
2020-08-27 19:58:53 +0800

    [refactor] [dong] feature : pass should return owe 1730 and earn 47 credits in html type when generate html statement given Customer BigCo has three performances. Hamlet has 55 audiences.'As You Like Is has 35 audiences. Othello has 40 audiences.

--- src/statement.js
+++ src/statement.js
@@ -1,8 +1,8 @@
-const {createStatementData} = require("./createStatementData");
+const { createStatementData } = require("./createStatementData");
 
-function statement (invoice, plays) {
+function statement(invoice, plays) {
   let result = generateTxtResult(createStatementData(invoice, plays));
-  return result; 
+  return result;
 }
 
 function generateTxtResult(data) {
@@ -16,12 +16,12 @@ function generateTxtResult(data) {
 function generatePerformInfoTxtResult(data, result) {
   for (let perf of data.performances) {
     const play = data.plays[perf.playID];
-    result += ` ${play.name}: ${formatUSD(calculateAmount(play, perf) )} (${perf.audience} seats)\n`;
+    result += ` ${play.name}: ${formatUSD(calculateAmount(play, perf))} (${perf.audience} seats)\n`;
   }
   return result;
 }
 
-function formatUSD(thisAmount){
+function formatUSD(thisAmount) {
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -52,7 +52,27 @@ function calculateAmount(play, perf) {
   }
   return thisAmount;
 }
+function htmlStatement(invoice, plays) {
+  let result = generateHtmlResult(createStatementData(invoice, plays));
+  return result;
+}
+function generateHtmlResult(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr>`
+  result = generatePerformInfoHtmlResult(data, result);
+  result += `</table>\n<p>Amount owed is <em>${formatUSD(data.totalAmount)}</em></p>\n`
+  result += `<p>You earned <em>${data.volumeCredits}</em> credits</p>\n`
+  return result;
+}
+
+function generatePerformInfoHtmlResult(data, result) {
+  for (let perf of data.performances) {
+    const play = data.plays[perf.playID]
+    result += ` <tr><td>${play.name}</td><td>${perf.audience}</td><td>${formatUSD(calculateAmount(play, perf))}</td></tr>\n`;
+  }
+  return result;
+}
 
 module.exports = {
   statement,
+  htmlStatement,
 };
